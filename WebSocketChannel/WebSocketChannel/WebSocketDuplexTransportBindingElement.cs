80f1b03479e906cd17e7fcb6f42abd852357639e
afeng302 <afeng302@gmail.com>
2016-01-11 16:32:03 +0800

    add solution

---
+++ WebSocketChannel/WebSocketChannel/WebSocketDuplexTransportBindingElement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+
+namespace WebSocketChannel
+{
+    public class WebSocketDuplexTransportBindingElement : TransportBindingElement
+    {
+        public const string WebSocketScheme = "ws";
+
+        public WebSocketDuplexTransportBindingElement()
+            : base()
+        {
+        }
+
+        public WebSocketDuplexTransportBindingElement(WebSocketDuplexTransportBindingElement other)
+            : base(other)
+        {
+        }
+        public override string Scheme
+        {
+            get { return WebSocketScheme; }
+        }
+
+        public override BindingElement Clone()
+        {
+            return new WebSocketDuplexTransportBindingElement(this);
+        }
+
+        public override bool CanBuildChannelFactory<TChannel>(BindingContext context)
+        {
+            return typeof(TChannel) == typeof(IDuplexChannel);
+        }
+    }
+}
